63d4d1a70a6f1d85e57dc6dbcea9ca6aad852d40
Craig Anderson <craigstuartanderson@gmail.com>
2017-12-06 09:44:25 -0500

    hand in stuff

--- dev/scripts/components/MarketPlace.js
+++ dev/scripts/components/MarketPlace.js
@@ -35,7 +35,7 @@ export default class MarketPlace extends React.Component {
     makeDataCall(keywords) {
         console.log(`Called with the keyword ${keywords}`)
         const access_key = 'MDo5ODRjMDU2Ni1kNTBhLTExZTctYjFmYS1lN2UwOGZlNzE3OWY6WFJBVXV1Q2FkWDdBUkQ5aUtxc0ZYejl3ZTVCaDU0emFYRG56';
-        axios.get(`http://lcboapi.com/products?`, {
+        axios.get(`https://lcboapi.com/products?`, {
             params: {
                 dataType: 'json',
                 q: keywords,
@@ -45,6 +45,7 @@ export default class MarketPlace extends React.Component {
             }
         }).then((res) => {
             console.log('result from API ',res.data.result);
+            console.log('result' , res);
             this.setState({
                 wineResults: res.data.result.filter(wine => (wine.primary_category === "Wine" && wine.sugar_content)),
                 showReply: true
